aafcacf1d0b91a0d412d367ef20b27d6d3e69ae3
nischalkarki <74295013+A4OnE@users.noreply.github.com>
2023-05-26 02:09:20 +0545

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -46,7 +46,7 @@ const swaggerOptions={
            servers:[4002]
        }
     },
-    apis:["./routes/*.ts",`${__dirname}/routes/*.routes.ts`,]
+     apis:["./routes/*.ts","./Routes/*.ts","./**/*.ts",'index.ts',`${__dirname}/routes/*.routes.ts`,`${__dirname}/Routes/*.routes.ts`]
    }
 const swaggerDocs=swaggerJsDoc(swaggerOptions);
 app.use('/doc',swaggerUiExpress.serve,swaggerUiExpress.setup(swaggerDocs));
